abd3877a8153b0037840f3b7898d6a204e9cc0d1
patience <97902172+abdulhaleem7@users.noreply.github.com>
2024-04-18 23:28:00 +0100

    commit

--- Models/Entities/Auditables.cs
+++ Models/Entities/Auditables.cs
@@ -4,7 +4,7 @@ namespace Ecommerce.Models.Entities
 	public class Auditables
 	{
 		public Guid Id { get; set; } = new Guid();
-		public bool IsDeleted { get; set; }
+		public bool IsDeleted { get; set; } 
 		public DateTime DateCreated { get; set; } = DateTime.Now;
 		public DateTime DateModified { get; set; }
 		public DateTime DateDeleted { get; set; }
