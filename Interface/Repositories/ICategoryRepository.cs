309cc761af4e86282e35f350ae6d3494b2362705
salaudeen abdulhaleem <salaudeenhaleem7@gmail.com>
2024-04-18 23:50:02 +0100

    commit

--- Interface/Repositories/ICategoryRepository.cs
+++
@@ -1,8 +0,0 @@
-using Ecommerce.Models.Entities;
-
-namespace Ecommerce.Interface.Repositories;
-
-public interface ICategoryRepository : IRepository<Category>
-{
-    
-}
--- Models/Enums/CompanyStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace Ecommerce.Models.Enums;
-
-public enum CompanyStatus
-{
-    Pending = 1,
-    Approved ,
-    Flag    
-}
